ad748dfb97ba9483a79eb61192d14effdfd4c748
John Titor <95267320+johntitor08@users.noreply.github.com>
2025-08-31 11:16:24 +0300

    f

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -1,22 +1,40 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoPizza.Models;
 using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoPizza.Pages
 {
-public class PizzaListModel : PageModel
-{
-    private readonly PizzaService _service;
-    public IList<Pizza> PizzaList { get;set; } = default!;
-
-    public PizzaListModel(PizzaService service)
+    public class PizzaListModel(PizzaService service) : PageModel
     {
-        _service = service;
-    }
+        private readonly PizzaService _service = service;
+        public IList<Pizza> PizzaList { get; set; } = default!;
 
-    public void OnGet()
-    {
-        PizzaList = _service.GetPizzas();
+        public void OnGet()
+        {
+            PizzaList = _service.GetPizzas();
+        }
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
+        }
     }
-}
 }
